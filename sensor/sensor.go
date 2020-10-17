7419df4eb00f5f4a3a12464f82846fdfcd787d05
Shauni <shaunisoul@protonmail.com>
2020-10-17 21:41:26 +0000

    feat(sensor): handle errors when creating template

--- sensor/sensor.go
+++ sensor/sensor.go
@@ -24,12 +24,18 @@ func Run(cfg config, generator GeneratorFunc) {
 				cfg.logFormat = "%.4f%s"
 			}
 			tmpl, err := template.New("log").Parse(cfg.logFormat)
-			// client.Publish(topic, 0, false, fmt.Sprintf("%d|%.4f", unix, data))
+
+			if err != nil {
+				log.Fatal("Error creating log template at sensor.run:", err)
+			}
+
 			data := struct {
 				Value float64
 				Unit  string
 			}{value, cfg.unit}
+
 			err = tmpl.Execute(log.Writer(), data)
+
 			if err != nil {
 				log.Fatal("Error executing log template at sensor.run:", err)
 			}

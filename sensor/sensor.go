b7f0fcdff70f1d4081245f3fb007f8b6de3823cf
Shauni <shaunisoul@protonmail.com>
2020-10-24 17:29:14 +0000

    docs(sensor): add documentation on each sensor definition and methods

--- sensor/sensor.go
+++ sensor/sensor.go
@@ -2,10 +2,15 @@ package sensor
 
 import "log"
 
+// A Sensor carries technical informations and exposes a value that can be read
+// in order to be used for computations.
 type Sensor interface {
+	// Value returns the current value of the sensor.
 	Value() []byte
 }
 
+// An emptySensor always has nil values. It is not struct{}, since vars of this
+// type must have distinct addresses.
 type emptySensor int
 
 func (*emptySensor) Value() []byte {
@@ -16,10 +21,17 @@ func NewSensor() Sensor {
 	return new(emptySensor)
 }
 
+// EmptySensor returns a non-nil, empty Sensor. It always has nil values. It is
+// typically used by initialization and tests.
 func EmptySensor() Sensor {
 	return new(emptySensor)
 }
 
+// A ValueMethod tells the sensor how the value is read.
+type ValueMethod func() []byte
+
+// WithCustomValue returns a copy of sensor that use the callback parameter to
+// read the sensor's value.
 func WithCustomValue(sensor Sensor, callback ValueMethod) Sensor {
 	if sensor == nil {
 		log.Panic("cannot create sensor from nil one")
@@ -30,12 +42,12 @@ func WithCustomValue(sensor Sensor, callback ValueMethod) Sensor {
 	return &customizedSensor
 }
 
+// newCustomSensor returns an initialized customSensor.
 func newCustomSensor(sensor Sensor, callback ValueMethod) customSensor {
 	return customSensor{Sensor: sensor, valueMethod: callback}
 }
 
-type ValueMethod func() []byte
-
+// A customSensor is a sensor with a defined value method.
 type customSensor struct {
 	Sensor
 
